10bb3a698946b8d18c7d8f76b737ba71c285e55a
Anthony <aretelew@gmail.com>
2022-04-26 21:45:05 -0500

    Changed Tank class from JLabel to JPanel with 2D graphics to offer more flexibility with design.

--- Tank.java
+++ Tank.java
@@ -1,17 +1,20 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Tank extends JLabel {
+public class Tank extends JPanel {
     private int width;
     private int height;
     public Tank(int width, int height, Color c) {
         this.setBounds(0, 0, width, height);
-        this.setBackground(c);
-        this.setOpaque(true);
         this.width = width;
         this.height = height;
-    }
 
+    }
+    public void paint(Graphics g) {
+        Graphics tank = (Graphics2D) g;
+        tank.setColor(Color.RED);
+        tank.fillOval(0,0, width, height);
+    }
     public int getWidth() {
         return width;
     }
