ccb4558d429fca66a5ed68af53f7d6b3c9653a27
Anthony <aretelew@gmail.com>
2022-04-26 21:58:31 -0500

    Fixed color issue.

--- Tank.java
+++ Tank.java
@@ -4,14 +4,15 @@ import javax.swing.*;
 public class Tank extends JPanel {
     private int width;
     private int height;
-    public Tank(int width, int height, Color c) {
+    private Color color;
+    public Tank(int width, int height, Color color) {
         this.setBounds(0, 0, width, height);
         this.width = width;
         this.height = height;
-
+        this.color = color;
     }
     public void paint(Graphics g) {
-        g.setColor(Color.RED);
+        g.setColor(color);
         g.fillOval(0,0, width, height);
     }
     public int getWidth() {
