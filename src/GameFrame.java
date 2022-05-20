3f91dbe936e9f42065562a44341bd3bde644423f
James X <xtremejames1@gmail.com>
2022-05-20 10:33:23 -0500

    TESTING ONLY

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -86,8 +86,9 @@ public class GameFrame{
         game(); //Starts game
     }
     public void game() {
-        panel = new GamePanel(game, net); //Creates GamePanel object
-        panel.setVisible(true);
-        frame.setContentPane(panel);
+        //panel = new GamePanel(game, net); //Creates GamePanel object
+        //panel.setVisible(true);
+        //frame.setContentPane(panel);
+        game.setSendData("testing");
     }
 }
