231e70277b074d8a09f5e31e2a1ddbc0b49fa33d
James X <xtremejames1@gmail.com>
2022-05-26 16:56:30 -0500

    TESTING ONLY

--- src/ChatThread.java
+++ src/ChatThread.java
@@ -1,14 +1,33 @@
+import javax.swing.*;
+
 public class ChatThread extends Thread{
     private GameInfo game;
+    private JFrame frame = new JFrame();
+    private JPanel panel;
+    private JTextArea text;
     public ChatThread(GameInfo g) {
         game = g;
+        panel = new JPanel();
+        text = new JTextArea();
+        frame.setLayout(null);
+        frame.setSize(1280, 720);
+        frame.setTitle("JamesChat");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.setResizable(true);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
     @Override
     public void run() {
+
         while(true) {
+            text.setText(game.getReceiveData());
+            /*
             System.out.println("\033[H\033[2J");
             System.out.flush();
             System.out.println(game.getReceiveData());
+            */
         }
     }
 }
